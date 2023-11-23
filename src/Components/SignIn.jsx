0c5fdf7e21267cc398a88bb474f5b322161ffae8
Jack Ryan <thisisjackryan1@gmail.com>
2023-11-23 11:23:01 -0600

    added firebase login authentication

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,17 +1,54 @@
-
+import { useState } from "react";
+import { auth } from "../Config/firebase-config";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 
 const SignIn = (props) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(auth?.currentUser?.email)
+
+    const singInUser = async () => {
+        try{
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err){
+            console.error(err);
+        }
+    }
+
+    const logOut = async () => {
+        try{
+            await signOut(auth)
+        } catch (err){
+            console.error(err);
+        }
+    }
+
+
     return (
         <div className="signIn">
             <div className="signInLabel">Sign in</div>
-            <input type="text" placeholder="Email or Number" />
-            <input type="password" placeholder="password" name="" id="" />
-            <button>Sign In</button>
+            <input 
+                type="text" 
+                placeholder="Email or Number"  
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder="password" 
+                id="" 
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={singInUser}>Sign In</button>
             <div>
                 <div className="swapSignIn" onClick={props.swap}>Don't have account? Create One!</div>
             </div>
+            <br />
+            <br />
+            <button onClick={logOut}>Log Out</button>
         </div>
     )
 }
---
+++ src/Config/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZjgosQcTQgNfvwAaFC_W6R5Sv0JjjxkM",
+  authDomain: "friendly-betting-fb47e.firebaseapp.com",
+  projectId: "friendly-betting-fb47e",
+  storageBucket: "friendly-betting-fb47e.appspot.com",
+  messagingSenderId: "897713635691",
+  appId: "1:897713635691:web:70b530052a319f77e14c2e",
+  measurementId: "G-74305KF5X4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export const auth = getAuth(app);
