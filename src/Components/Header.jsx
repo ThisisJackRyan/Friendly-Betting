9de5b1b66370b8388c12b454a68fc426d590f007
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-11-28 23:01:19 -0600

    checking if user is checked in or not

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 import { CSSTransition } from 'react-transition-group';
 
 
 
 const Header = () => {
+    const location = useLocation();
+
     return(
         <div className="header">
             <div className="x1">
@@ -13,7 +15,11 @@ const Header = () => {
             </div>
             <div className="x1">Title of website</div>
             <div className="flex pages x1">
-                <Link className="Link" to="/SignIn">signIn</Link>
+            {location.pathname !== "/SignIn" ? <Link className="Link" to="/SignIn">Sign In</Link>: <Link className="Link" to="/">Sign In</Link>
+            
+            
+            
+            }
                 <Link className="Link" to="/NewBet">Create New Bet</Link>
             </div>
         </div>
--- src/Components/MainPages/Welcome.jsx
+++ src/Components/MainPages/Welcome.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { useState } from 'react';  
+import { useState, useEffect } from 'react';  
+import { auth } from "../../Config/firebase-config";
+import { Link } from "react-router-dom";
+
+
 
 const Welcome = () => {
 
-    const [signUp, setSignUp] = useState(true);
+    const [signedIn, setSignedIn] = useState(true);
 
-    const toggleForm = () => {
-        setSignUp((current) => !current)
-      }
+    console.log(auth?.currentUser?.email)
+
+    useEffect(() => {
+        isAuthenticated();
+    }, []);
+
+    const isAuthenticated = () => {
+        const currentUserEmail = auth?.currentUser?.email;
+        const isAuthenticated = currentUserEmail !== undefined;
+        setSignedIn(isAuthenticated);
+    };
+
     return (
+    <div>
+        {signedIn ? (
         <div>
-            <div>Hello</div>
+            
+            {/* Render the authenticated content */}
+            <h1>Welcome, User!</h1>
+            <p>This is the authenticated content.</p>
         </div>
+        ) : (
+        <div>
+            {/* Render the non-authenticated content */}
+            <h1>Please sign in to access this content.</h1>
+            <Link className="Link" to="/SignIn">Sign In</Link>
+        </div>
+        )}
+    </div>
     );
 };
 
