6c490ef73cfc30597d010e15d68f746aabfaf037
Jack Ryan <thisisjackryan1@gmail.com>
2024-03-15 17:12:47 -0500

    centers create bet button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -23,29 +23,31 @@ const Header = () => {
     return(
         <div className="sticky top-0 bottom-0 right-0 left-0 w-full flex justify-between">
             <Link className="ease-in no-underline text-black" to="Friendly-Betting/">
-            <div className=" bg-spring-green-light box-shadow rounded-br-2xl p-4 pt-8 text-shadow flex justify-start items-center text-5xl">
-                <span className="font-sans font-thin">FR</span>
-                <span className="font-serif font-bold text-2xl">IE</span>
-                <span className="font-sans font-thin">NDLY &nbsp;</span>
-                <span>Bets</span>
-            </div>
-            </Link>
-            
-            <CSSTransition
-                in={(location.pathname === "/Friendly-Betting/Bet")}
-                timeout={300}
-                classNames="createBet"
-                unmountOnExit              
-            >
-                <div>
-                    <div onClick={swapDisplay} className="h-full flex bg-spring-green-light box-shadow rounded-b-2xl pl-8 pr-8 justify-center items-center">
-                        <span className="flex justify-center items-center">Create One!</span>
-                    </div>
+                <div className=" bg-spring-green-light box-shadow rounded-br-2xl p-4 pt-8 text-shadow flex justify-start items-center text-5xl">
+                    <span className="font-sans font-thin">FR</span>
+                    <span className="font-serif font-bold text-2xl">IE</span>
+                    <span className="font-sans font-thin">NDLY &nbsp;</span>
+                    <span>Bets</span>
                 </div>
-                
-            </CSSTransition>
+            </Link>
+            <div className="h-full absolute-center-x">
+                <CSSTransition
+                    in={(location.pathname === "/Friendly-Betting/Bet")}
+                    timeout={300}
+                    classNames="createBet"
+                    unmountOnExit              
+                >
+                    <div className="h-full">
+                        <div onClick={swapDisplay} className="h-full flex bg-spring-green-light box-shadow rounded-b-2xl pl-8 pr-8 justify-center items-center">
+                            <span className="flex justify-center items-center">Create One!</span>
+                        </div>
+                    </div>
+                    
+                </CSSTransition>
+            </div>
+            
     
-            <Link className="flex box-shadow  bg-spring-green-light rounded-bl-2xl betLink p-8 pb-4 no-underline text-black" to="Friendly-Betting/Bet"><span className="flex items-center justify-center">View Bets</span></Link>
+            <Link className="flex box-shadow bg-spring-green-light rounded-bl-2xl betLink pt-8 pl-32 pr-32 pb-4 no-underline text-black" to="Friendly-Betting/Bet"><span className="flex items-center justify-center">View Bets</span></Link>
             {display ? <BetNav swap={swapDisplay}/> : null}
 
         </div>
