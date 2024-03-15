b89b05a187c710dfec7e10c2b9a8bcea3731e4e0
Jack Ryan <thisisjackryan1@gmail.com>
2024-03-15 16:25:52 -0500

    small fixes on display bet and clean up css

--- src/Components/Bets/DisplayBet.jsx
+++ src/Components/Bets/DisplayBet.jsx
@@ -20,20 +20,20 @@ const DisplayBet = (props) => {
                 className="text-black no-underline"
             
             >
-                <div className={`flex justify-between box-shadow bg-spring-green-light rounded-2xl p-4 m-4 ${css.bet}`}>
-                    <div key={props.bet.id}>
-                        <p>{props.bet.type}</p>
+                <div className="flex justify-between box-shadow bg-spring-green-light rounded-2xl p-4 m-4">
+                    <div className="flex flex-col gap-6 max-w-sm"key={props.bet.id}>
+                        <div>{props.bet.type}</div>
                         <div className='text-2xl font-bold text-center pl-8'>{props.bet.bet}</div>
-                        <p className='text-lg pl-8'>Line 13.5</p>
-                        <p className='text-sm pl-8'>Created by <span className='font-semibold'>{props.bet.createdByEmail}</span></p>
+                        <div className='text-lg pl-8'>Line 13.5</div>
+                        <div className='text-sm pl-8'>Created by <span className='font-semibold'>{props.bet.createdByEmail}</span></div>
                     </div>
-                    <div>
+                    <div className=''>
                         <div className={`flex`}>
-                            <div className={`flex flex-col p-4  ${css.line}`}>
+                            <div className={`flex flex-col p-2 m-2  ${css.line}`}>
                                 <div className='flex  justify-center items-center p-4 font-semibold'>Chiefs</div>
                                 <div className='flex justify-center items-center p-4 pb-0 font-light'> 27 Bets</div>
                             </div>
-                            <div className='flex flex-col p-4'>
+                            <div className='flex flex-col p-2 m-2'>
                                 <div className='flex justify-center items-center p-4 font-semibold'>Chargers</div>
                                 <div className='flex justify-center items-center p-4 pb-0 font-light'>23 Bets</div>
                             </div>
