da1b4275dc3a8dbf2c3ab6f2ac6441acff06d06c
Jack Ryan <thisisjackryan1@gmail.com>
2024-03-15 22:42:55 -0500

    changes the header styleing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
     return(
         <div className="sticky top-0 bottom-0 right-0 left-0 w-full flex justify-between">
             <Link className="ease-in no-underline text-black" to="Friendly-Betting/">
-                <div className=" bg-spring-green-light box-shadow rounded-br-2xl p-4 pt-8 text-shadow flex justify-start items-center text-5xl">
+                <div className="p-4 pt-8 text-shadow flex justify-start items-center text-5xl">
                     <span className="font-sans font-thin">FR</span>
                     <span className="font-serif font-bold text-2xl">IE</span>
                     <span className="font-sans font-thin">NDLY &nbsp;</span>
@@ -44,7 +44,11 @@ const Header = () => {
                     </div>
                 </CSSTransition>
             </div>
-            <Link className="flex box-shadow bg-spring-green-light rounded-bl-2xl betLink pt-8 pl-32 pr-32 pb-4 no-underline text-black" to="Friendly-Betting/Bet"><span className="flex items-center justify-center">View Bets</span></Link>
+        
+                <Link className="flex rounded-bl-2xl betLink pt-8 p-4 no-underline text-black" to="Friendly-Betting/Bet" >
+                    <span className="flex items-center justify-center z-10">View Bets</span>
+                </Link>
+
             {display ? <BetNav swap={swapDisplay}/> : null}
 
         </div>
