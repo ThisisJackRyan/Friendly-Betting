75156f1b53ecb66b75d72d130b3e1bc42bb4b92b
Jack Ryan <thisisjackryan1@gmail.com>
2024-04-02 10:14:31 -0500

    cleans up code

--- src/Components/Bets/BetNav.jsx
+++ src/Components/Bets/BetNav.jsx
@@ -11,27 +11,8 @@ const BetNav = (props) => {
     const goTo = () => {
         navigate(`/Friendly-Betting/${betType}`)
     }
-    const goToOverUnder = () => {
-        navigate('/Friendly-Betting/OverUnderBets')
-    }
-    const goToProps = () => {
-        navigate('/Friendly-Betting/PropBets')
-    }
   return (
     <div className="fixed-center bg-spring-green-light text-black p-4 rounded-md black-border box-shadow-no-hover">
-        {/* <div className="x"><span onClick={props.swap} >x</span></div>
-        <div className='text-3xl flex justify-center items-center mb-4'><span className='border-bottom'>Bets</span></div>
-        <div className="flex justify-center items-center flex-col gap-4 m-4">
-            <div>
-                <div onClick={goToMoneyLine} className={css.betNavLinks} >Money Line</div>
-            </div>
-            <div>
-                <div onClick={goToOverUnder} className={css.betNavLinks} >Over / Under</div>
-            </div>
-            <div>
-                <div onClick={goToProps} className={css.betNavLinks} >Prop</div>
-            </div>
-        </div> */}
         <div className='flex justify-end ml-4'>
             <div className="x text-md"><span onClick={props.swap} >x</span></div>
         </div>
---
+++ src/Components/Bets/CreateBet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateBet = () => {
+    // Your component logic goes here
+
+    return (
+        <div>
+            {/* Your JSX code goes here */}
+        </div>
+    );
+};
+
+export default CreateBet;
