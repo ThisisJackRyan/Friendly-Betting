1c3498dcc6916af8730a183438c8732b0fcfc96e
Jack Ryan <thisisjackryan1@gmail.com>
2024-04-11 10:06:04 -0500

    fixes share ui

--- src/Components/Bets/Players/ShareBet.jsx
+++ src/Components/Bets/Players/ShareBet.jsx
@@ -19,27 +19,56 @@ const ShareBet = (props) => {
     }
 
     return (
-        <div className={css.shareBet}>
-            <div className="flex justify-end mb-2"><span className={css.x} onClick={props.display}>x</span></div>
-            <div className={`justify-center text-center ${css.friendsLabel}`}><span>Share</span></div>
-            
-            <div className={css.border}>
-                <QRCode
-                    title="qr-code"
-                    value={window.location.href}
-                    bgColor="#FFFFFF"
-                    fgcolor="#000000"
-                    size="254"
-                />
-            </div>
-            <div className='flex  max-w-64 m-4'>
-                <div className={css.urlCopy}>
-                    {window.location.href}
+        
+        <div>
+            <div className='fixed-center text-2xl p-8 px-16 bg-spring-green-light rounded-md box-shadow-no-hover'>
+                <div className="flex justify-end mb-2 "><span className={css.x} onClick={props.display}>x</span></div>
+                <div className='justify-center text-center text-4xl mb-6 '><span className='border-bottom'>Share</span></div>
+                <div className={css.border}>
+                    <QRCode
+                        title="qr-code"
+                        value={window.location.href}
+                        bgColor="#FFFFFF"
+                        fgcolor="#000000"
+                        size="254"
+                    />
                 </div>
-                <button className={css.copyButton} onClick={copyToClipboard}>Copy</button>
+                <div className='flex max-w-64 mt-4'>
+                    <div className={css.urlCopy}>
+                            {window.location.href}
+                        </div>
+                    <button className={css.copyButton} onClick={copyToClipboard}>Copy</button>
+                </div>
+                    
             </div>
-
         </div>
+
+
+
+
+
+
+        // <div className={`bg-secondary-spring-green-light ${css.shareBet}`}>
+        //     <div className="flex justify-end mb-2"><span className={css.x} onClick={props.display}>x</span></div>
+        //     <div className={`justify-center text-center ${css.friendsLabel}`}><span>Share</span></div>
+            
+        //     <div className={css.border}>
+        //         <QRCode
+        //             title="qr-code"
+        //             value={window.location.href}
+        //             bgColor="#FFFFFF"
+        //             fgcolor="#000000"
+        //             size="254"
+        //         />
+        //     </div>
+        //     <div className='flex  max-w-64 m-4'>
+        //         <div className={css.urlCopy}>
+        //             {window.location.href}
+        //         </div>
+        //         <button className={css.copyButton} onClick={copyToClipboard}>Copy</button>
+        //     </div>
+
+        // </div>
         
     );
 };
