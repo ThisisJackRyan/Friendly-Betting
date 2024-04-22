2a9c08274225ff3956b505fc32dfeffe8ba150e1
Jack Ryan <thisisjackryan1@gmail.com>
2024-04-22 10:56:40 -0500

    adds ability to edit over under

--- src/Components/Bets/OverUnderBets/CreateOverUnder.jsx
+++ src/Components/Bets/OverUnderBets/CreateOverUnder.jsx
@@ -1,42 +1,73 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation} from 'react-router-dom';
 import { getSignedInUserInfo, isUserSignedIn } from '../../../Config/base';
 import {useState, useEffect} from 'react';
 import {db} from '../../../Config/firebase-config';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc, updateDoc } from 'firebase/firestore';
 
 const CreateOverUnder = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     
     const [bet, setBet] = useState('');
     const [line, setLine] = useState(0);
+
+    const [isLocationNull, setIsLocationNull] = useState(location.state === null);
+
 
     useEffect(() => {
         if(!isUserSignedIn()){
             alert("You must be signed in to create a bet")
             navigate(`/Friendly-Betting/Bet`)
         }
+        if(isLocationNull == false){
+            try{
+                setBet(location.state.bets.bet)
+                setLine(location.state.bets.line)
+            }
+            catch (e){
+                console.error(e);
+            }
+        }
     }, [])
+
+    const updateBet = async () => {
+        const betsDocRef = doc(db, "bets", location.state.betUrl.id);
+        const betsDocSnap = await getDoc(betsDocRef);
+
+            await updateDoc(doc(db,"OverUnderBets" , betsDocSnap.data().betID), {
+                bet: bet,
+                line: line,
+            })
+            navigate(`/Friendly-Betting/Bet/OverUnderBets/${location.state.betUrl.id}/`) 
+    }
+    const createBet = async (userInfo) => {
+        const betRef = await addDoc(collection(db, "OverUnderBets"), {
+            bet: bet,
+            line: line,
+        })
+        const betLocation = await addDoc(collection(db, "bets"), {
+            betID: betRef.id,
+            type: "Over Under",
+            bet: bet,
+            createdByID: userInfo["uid"],
+            createdByEmail: userInfo["email"],
+            under: 0,
+            over: 0,
+        })
+    navigate(`/Friendly-Betting/Bet/OverUnderBets/${betLocation.id}/`) 
+    }
 
     const handelSubmit = async (e) => {
         e.preventDefault();
         try {
             const userInfo = getSignedInUserInfo();
             if(userInfo !== null) {
-                const betRef = await addDoc(collection(db, "OverUnderBets"), {
-                    bet: bet,
-                    line: line,
-                })
-                const betLocation = await addDoc(collection(db, "bets"), {
-                    betID: betRef.id,
-                    type: "Over Under",
-                    bet: bet,
-                    createdByID: userInfo["uid"],
-                    createdByEmail: userInfo["email"],
-                    under: 0,
-                    over: 0,
-                })
-            navigate(`/Friendly-Betting/Bet/OverUnderBets/${betLocation.id}/`) 
+                if(isLocationNull){
+                    createBet(userInfo)
+                } else {
+                    updateBet()
+                }
             } else {
                 alert("You must be signed in to create a bet")
             }
@@ -61,6 +92,7 @@ const CreateOverUnder = () => {
                             className='bg-secondary-spring-green-light rounded-md BetTextArea p-4'
                             onChange={(e) => setBet(e.target.value)}
                             placeholder='How many roles will grandpa eat at the family dinner?'
+                            value={bet}
                         >
                         </textarea>
                     </div>
@@ -76,6 +108,7 @@ const CreateOverUnder = () => {
                             step="0.5"
                             onChange={(e) =>setLine(Number(e.target.value))}
                             placeholder='13.5'
+                            value={line}
                         />
                     </div>
                 </div>
